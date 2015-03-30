7e23dd0c55e493ae849410783a0a82241df7d32f
Daniel Choi <dhchoi@gmail.com>
2015-03-30 13:44:22 -0400

    multi merging proof of concept

--- Merge.hs
+++ Merge.hs
@@ -3,25 +3,30 @@ import System.Environment
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Maybe
 import Text.XML.HXT.Core
+import Text.XML.HXT.Arrow.XmlArrow
 
 main = do
-    [layout,content] <- getArgs
-    processTemplateWithLayout layout content "content" >> return ()
+    let layout = "layout.html"
+    let xs = [("content.html", "content"), ("sidebar.html", "sidebar")]
+    runX (
+      (
+        foldl (\acc x -> acc >>> mergeTemplates x) 
+            (readDocument [withValidate no, withParseHTML yes, withInputEncoding utf8] layout)
+            xs
+      )
+      >>>
+      writeDocument [withIndent yes, withOutputHTML, withXmlPi no] "-"
+      )
 
 
-processTemplateWithLayout layoutFile contentFile idName = runX (
-    (
+-- mergeTemplates :: ArrowXml a => (String, String) -> a XmlTree XmlTree
+mergeTemplates (contentFile, idName) = 
       (\contentXml -> 
-          readDocument [withValidate no, withParseHTML yes, withInputEncoding utf8] layoutFile
-          >>> 
           processTopDown ((constA contentXml) `when` (isElem >>> hasName "yield" >>> hasAttrValue "id" (== idName)))
       )
       $< (readDocument [withValidate no, withParseHTML yes, withInputEncoding utf8] contentFile 
           >>> (deep (isElem >>> hasAttrValue "id" (== idName))) `when` isElem)
-    )
-    >>>
-    writeDocument [withIndent yes, withOutputHTML, withXmlPi no] "-"
-    )
+    
 
 
 
